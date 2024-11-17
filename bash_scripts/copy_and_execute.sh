0c4fa568845cd4ee35da7fdcb65209d74547aec1
Pranav5Murali <154644683+Pranav5Murali@users.noreply.github.com>
2024-11-17 14:08:24 +0530

    Update copy_and_execute.sh

--- bash_scripts/copy_and_execute.sh
+++ bash_scripts/copy_and_execute.sh
@@ -2,11 +2,10 @@
 ssh_pwd="$1"
 
 echo "Connecting to the remote machine and copy script"
-sshpass -p "$ssh_pwd"\
-scp python_scripts/info1.py \
-karthik@192.168.1.105:/home/karthik/target
+sshpass -p "$ssh_pwd" \
+scp python_scripts/info1.py karthik@192.168.1.105:/home/karthik/target
 
 echo "executing the python script on the remote machine"
-sshpass -p "$ssh_pwd"\
-ssh karthik@192.168.1.105\
+sshpass -p "$ssh_pwd" \
+ssh karthik@192.168.1.105 \
 'cd /home/karthik/target && python3 info1.py'
