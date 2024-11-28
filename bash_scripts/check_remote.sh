953dcf5218945232688407d78f63a1f4e2b216dd
Pranav5Murali <154644683+Pranav5Murali@users.noreply.github.com>
2024-11-28 08:13:35 +0530

    Update check_remote.sh

--- bash_scripts/check_remote.sh
+++ bash_scripts/check_remote.sh
@@ -9,7 +9,7 @@ REMOTE_IP="192.168.1.105"
 
 # Run the commands on the remote machine using sshpass
 echo "Connecting to remote machine..."
-sshpass -p "$SSH_PASSWORD" ssh "$REMOTE_USER@$REMOTE_IP" <<EOF
+sshpass -p "$SSH_PASSWORD" ssh -o StrictHostKeyChecking=no "$REMOTE_USER@$REMOTE_IP" <<EOF
 echo "Running commands on remote machine:"
 whoami
 ip a
