6575d92c693b4b27cd1aaf2d8acc896cb3498631
Pranav5Murali <154644683+Pranav5Murali@users.noreply.github.com>
2025-12-28 21:37:28 +0530

    Update check_remote.sh

--- bash_scripts/check_remote.sh
+++ bash_scripts/check_remote.sh
@@ -5,7 +5,7 @@ SSH_PASSWORD="$1"
 
 # Define the remote user and IP address
 REMOTE_USER="user1"
-REMOTE_IP="192.168.1.105"
+REMOTE_HOST="ubuntu-target" 
 
 # Run the commands on the remote machine using sshpass
 echo "Connecting to remote machine..."
