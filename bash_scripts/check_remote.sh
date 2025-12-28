22d1e695bd8f0a57c0acbdc8111028216c873474
Pranav5Murali <154644683+Pranav5Murali@users.noreply.github.com>
2025-12-28 21:57:06 +0530

    Update check_remote.sh

--- bash_scripts/check_remote.sh
+++ bash_scripts/check_remote.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
+set -e
 
 # Get the password from the argument
 SSH_PASSWORD="$1"
 
-# Define the remote user and IP address
+# Remote target details
 REMOTE_USER="user1"
-REMOTE_HOST="ubuntu-target" 
+REMOTE_HOST="ubuntu-target"
 
-# Run the commands on the remote machine using sshpass
 echo "Connecting to remote machine..."
-sshpass -p "$SSH_PASSWORD" ssh -o StrictHostKeyChecking=no "$REMOTE_USER@$REMOTE_IP" <<EOF
+
+sshpass -p "$SSH_PASSWORD" ssh \
+  -o StrictHostKeyChecking=no \
+  "$REMOTE_USER@$REMOTE_HOST" <<EOF
 echo "Running commands on remote machine:"
 whoami
+hostname
 ip a
 EOF
